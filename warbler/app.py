40072a18b2a1770d0970e8782a298f4d03a47d93
Alex <stavrosalexander13@gmail.com>
2023-10-18 20:27:59 -0400

    Finalized warbler project

--- warbler/app.py
+++ warbler/app.py
@@ -1,10 +1,10 @@
 import os
 
-from flask import Flask, render_template, request, flash, redirect, session, g
+from flask import Flask, render_template, request, flash, redirect, session, g, abort
 from flask_debugtoolbar import DebugToolbarExtension
 from sqlalchemy.exc import IntegrityError
 
-from forms import UserAddForm, LoginForm, MessageForm
+from forms import UserAddForm, LoginForm, MessageForm, UserEditForm
 from models import db, connect_db, User, Message
 
 CURR_USER_KEY = "curr_user"
@@ -209,13 +209,69 @@ def stop_following(follow_id):
 
     return redirect(f"/users/{g.user.id}/following")
 
+@app.route('/users/<int:user_id>/likes', methods=["GET"])
+def show_likes(user_id):
+    if not g.user:
+        flash("Access unauthorized.", "danger")
+        return redirect("/")
+
+    user = User.query.get_or_404(user_id)
+    return render_template('users/likes.html', user=user, likes=user.likes)
+
+@app.route('/messages/<int:message_id>/like', methods=['POST'])
+def add_like(message_id):
+    """Toggle a liked message for the currently-logged-in user."""
+
+    if not g.user:
+        flash("Access unauthorized.", "danger")
+        return redirect("/")
+
+    liked_message = Message.query.get_or_404(message_id)
+    if liked_message.user_id == g.user.id:
+        return abort(403)
+
+    user_likes = g.user.likes
+
+    if liked_message in user_likes:
+        g.user.likes = [like for like in user_likes if like != liked_message]
+    else:
+        g.user.likes.append(liked_message)
+
+    db.session.commit()
+
+    return redirect("/")
+
 
 @app.route('/users/profile', methods=["GET", "POST"])
 def profile():
     """Update profile for current user."""
 
-    # IMPLEMENT THIS
+    if not g.user:
+        flash("Access unauthorized.", "danger")
+        return redirect("/")
+    
+    form = UserEditForm()
+    user = g.user
 
+    if form.validate_on_submit():
+    
+        valid = User.authenticate(user.username, form.password.data)
+
+        if valid:
+            user.username = form.username.data or user.username
+            user.email = form.username.data or user.email
+            user.bio = form.bio.data or user.bio
+            user.image_url = form.image_url.data or user.image_url
+            user.header_image_url = form.header_image_url.data or user.header_image_url
+
+            db.session.commit()
+
+            flash("You have successfully updated your profile.")
+            return redirect(f'/users/{g.user.id}')
+        
+        flash("Invalid credentials.", 'danger')
+    
+    return render_template('/users/edit.html', form=form)
 
 @app.route('/users/delete', methods=["POST"])
 def delete_user():
@@ -295,8 +351,11 @@ def homepage():
     """
 
     if g.user:
+        followed_user_ids = [user.id for user in g.user.following]
+        user_ids_to_query = followed_user_ids + [g.user.id]
         messages = (Message
                     .query
+                    .filter(Message.user_id.in_(user_ids_to_query))
                     .order_by(Message.timestamp.desc())
                     .limit(100)
                     .all())
--- warbler/forms.py
+++ warbler/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, TextAreaField
-from wtforms.validators import DataRequired, Email, Length
+from wtforms.validators import DataRequired, Email, Length, Optional
 
 
 class MessageForm(FlaskForm):
@@ -23,3 +23,13 @@ class LoginForm(FlaskForm):
 
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[Length(min=6)])
+
+class UserEditForm(FlaskForm):
+    """Form for editing user"""
+
+    username = StringField('Username',)
+    email = StringField('E-mail', validators=[Email(), Optional()])
+    image_url = StringField('(Optional) Image URL')
+    header_image_url = StringField('(Optional) Header Image URL')
+    bio = StringField('(Optional) Bio')
+    password = StringField('Password for Verification')
